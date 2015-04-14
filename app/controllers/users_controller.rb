c5cc7b7f11432454c6439ed5a3cc8dd7546c6970
bennylin77 <bennylin77@gmail.com>
2015-04-14 19:59:17 +0800

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      accessible = [ :first_name, :last_name, :email, :phone_no, :postal, :county, :district, :address] # extend with your own params
+      accessible = [ :first_name, :last_name, :phone_no, :postal, :county, :district, :address] # extend with your own params
       accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
       params.require(:user).permit(accessible)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   
   has_attached_file :avatar, 
                     styles: { original: "300x300>" },
-                    default_url: "/images/:style/missing.png"  
+                    default_url: "default_avatar.png"  
   validates_attachment :avatar, 
                        content_type: { content_type: /\Aimage\/.*\Z/, message: "圖片格式錯誤" }, 
                        size: { less_than: 10.megabytes, message: "圖片大小超過10MB" }  
