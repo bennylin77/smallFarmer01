88aadd0498eeba89880abaa68d15bf46cad1aa47
bennylin77 <bennylin77@gmail.com>
2015-05-19 23:16:02 +0800

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def coupons
+    coupons = 0
+    current_user.coupons.each do |c|
+      coupons = coupons + c.amount        
+    end 
+    coupons.to_i
+  end
+  
+  def carts
+    quantity = 0
+    current_user.carts.each do |c| 
+      quantity = quantity + c.quantity
+    end 
+    quantity == 0 ? "" : quantity   
+  end  
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,19 +1,2 @@
 module MainHelper
-  
-  def coupons
-    coupons = 0
-    current_user.coupons.each do |c|
-      coupon = coupon + c.amount        
-    end 
-    coupons.to_i
-  end
-  
-  def carts
-    quantity = 0
-    current_user.carts.each do |c| 
-      quantity = quantity + c.quantity
-    end 
-    quantity ==0 ? "":quantity   
-  end
-  
 end
