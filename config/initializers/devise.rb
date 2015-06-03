725e112cd50fddd5ef8c0d0015797a03859973e3
bennylin77 <bennylin77@gmail.com>
2015-06-03 20:52:26 +0800

    update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   
   config.omniauth :facebook, "918735938148015", "066edc1a05776d4c88c8c3eb4b744be3",
-                  scope: 'email', image_size: 'normal'  
+                  scope: 'email', image_size: 'large'  
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
