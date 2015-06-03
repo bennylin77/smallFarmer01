f6a0f9b29aa0eddf6b0950d2bf12beff78454b2b
bennylin77 <bennylin77@gmail.com>
2015-06-03 19:57:38 +0800

    update

--- app/models/company_image.rb
+++ app/models/company_image.rb
@@ -2,7 +2,7 @@ class CompanyImage < ActiveRecord::Base
   belongs_to :company
   
   has_attached_file :image, 
-                    styles: { medium: "800x800>" },
+                    styles: { medium: "400x400>" },
                     default_url: "/images/:style/missing.png"  
   validates_attachment :image, 
                        content_type: { content_type: /\Aimage\/.*\Z/, message: "圖片格式錯誤" }, 
