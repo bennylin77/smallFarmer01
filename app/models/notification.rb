711f98b9a234497617b44198d4de3f3477b7c8c8
bennylin77 <bennylin77@gmail.com>
2015-08-05 17:43:32 +0800

    update

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ActiveRecord::Base
   belongs_to :order
   belongs_to :user
   belongs_to :comment
+  belongs_to :sub_comment  
   belongs_to :product
   belongs_to :invoice
 end
