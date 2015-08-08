dcd0f082ec087af037c6aed4db4a2ca84fa1b55d
bennylin77 <bennylin77@gmail.com>
2015-08-09 01:56:25 +0800

    update

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,8 @@ module ProductsHelper
   def unitOptions
     [['台斤', GLOBAL_VAR['UNIT_CATTY']], 
      ['公斤', GLOBAL_VAR['UNIT_KG']],
-     ['顆', GLOBAL_VAR['UNIT_STAR']]]
+     ['顆', GLOBAL_VAR['UNIT_STAR']],
+     ['包', GLOBAL_VAR['UNIT_BAO']]]
   end
   
   def reviewBg(score)
