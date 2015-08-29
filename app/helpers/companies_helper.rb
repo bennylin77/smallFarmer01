48ea89c1db7e406b98ac2c58d6a4e0b212eb5bd1
bennylin77 <bennylin77@gmail.com>
2015-08-30 00:07:05 +0800

    update

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,10 @@
 module CompaniesHelper
+  def farmSumOfDeliciousAndTasty(farm) 
+    sum = 0
+    farm.products.each do |pp|
+      sum = pp.product_boxings.first.orders.where('review_at IS NOT NULL and review_score = ? ', GLOBAL_VAR['ORDER_REVIEW_DELICIOUS']).count + 
+      pp.product_boxings.first.orders.where('review_at IS NOT NULL and review_score = ? ', GLOBAL_VAR['ORDER_REVIEW_TASTY']).count    
+    end  
+    sum  
+  end  
 end
