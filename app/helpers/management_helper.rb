f8f9b6daa7e16da919865b83a85122d441248341
bennylin77 <bennylin77@gmail.com>
2015-09-05 16:57:55 +0800

    update

--- app/helpers/management_helper.rb
+++ app/helpers/management_helper.rb
@@ -10,13 +10,13 @@ module ManagementHelper
     Time.now.year.downto(2015){ |i| 
       if i!=Time.now.year
         12.downto(1){ |j| 
-          options<<[ Date.civil( i, j, 16).midnight.strftime("%Y-%m-%d")+'~'+(( Date.civil(i, j, -1)+1 ).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 16).midnight]        
-          options<<[ Date.civil( i, j, 1).midnight.strftime("%Y-%m-%d")+'~'+(Date.civil(i, j, 16).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 1).midnight]
+          options<<[ Date.civil( i, j, 16).midnight.strftime("%Y-%m-%d")+'~'+(( Date.civil(i, j, -1)+1 ).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 16).midnight.strftime('%Y-%m-%d %H:%M:%S')]        
+          options<<[ Date.civil( i, j, 1).midnight.strftime("%Y-%m-%d")+'~'+(Date.civil(i, j, 16).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 1).midnight.strftime('%Y-%m-%d %H:%M:%S')]
         }
       else
         Time.now.month.downto(1){ |j| 
-          options<<[ Date.civil( i, j, 16).midnight.strftime("%Y-%m-%d")+'~'+(( Date.civil(i, j, -1)+1 ).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 16).midnight]        
-          options<<[ Date.civil( i, j, 1).midnight.strftime("%Y-%m-%d")+'~'+(Date.civil(i, j, 16).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 1).midnight]
+          options<<[ Date.civil( i, j, 16).midnight.strftime("%Y-%m-%d")+'~'+(( Date.civil(i, j, -1)+1 ).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 16).midnight.strftime('%Y-%m-%d %H:%M:%S')]        
+          options<<[ Date.civil( i, j, 1).midnight.strftime("%Y-%m-%d")+'~'+(Date.civil(i, j, 16).midnight-1).strftime("%Y-%m-%d"), Date.civil( i, j, 1).midnight.strftime('%Y-%m-%d %H:%M:%S')]
         }        
       end    
     }  
