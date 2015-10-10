8376d064ee4b274df9b9c3c0d370ebcbb116c049
bennylin77 <bennylin77@gmail.com>
2015-10-10 16:52:51 +0800

    update

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,7 +6,7 @@ class Company < ActiveRecord::Base
 
   has_attached_file :cover, 
                     styles: { original: "1024", medium: "500"},
-                    default_url: ActionController::Base.helpers.asset_path('default_company_cover.jpg')               
+                    default_url: ':company_missing'              
   validates_attachment :cover, 
                        content_type: { content_type: /\Aimage\/.*\Z/, message: "圖片格式錯誤" }, 
                        size: { less_than: 10.megabytes, message: "圖片大小超過10MB" }  
