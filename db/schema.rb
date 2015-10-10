26236b9982fcb57c3b85496ab7fea35162d7f69f
Benny <bennylin77@gmail.com>
2015-10-10 08:04:25 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006091551) do
+ActiveRecord::Schema.define(version: 20150927101521) do
 
   create_table "addresses", force: true do |t|
     t.integer  "user_id"
@@ -199,17 +199,10 @@ ActiveRecord::Schema.define(version: 20151006091551) do
 
   create_table "keywords", force: true do |t|
     t.string   "content"
-    t.integer  "kind",               default: 0,     null: false
-    t.integer  "search_count",       default: 0,     null: false
+    t.integer  "kind",         default: 0, null: false
+    t.integer  "search_count", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cover_file_name"
-    t.string   "cover_content_type"
-    t.integer  "cover_file_size"
-    t.datetime "cover_updated_at"
-    t.text     "description"
-    t.boolean  "available_c",        default: false, null: false
-    t.datetime "available_at"
   end
 
   create_table "notifications", force: true do |t|
