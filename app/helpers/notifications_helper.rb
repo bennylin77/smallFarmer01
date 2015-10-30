de3bcde40c70f9f182e4f152f105d1a63b5a8944
bennylin77 <bennylin77@gmail.com>
2015-10-31 00:52:03 +0800

    update

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -4,7 +4,7 @@ module NotificationsHelper
     [['留言', GLOBAL_VAR['NOTIFICATION_COMMENT']],
      ['訂單', GLOBAL_VAR['NOTIFICATION_PRODUCT']], 
      ['系統', GLOBAL_VAR['NOTIFICATION_SYSTEM']],
-     ['回饋金', GLOBAL_VAR['NOTIFICATION_PROMOTION']]]
+     ['評價', GLOBAL_VAR['NOTIFICATION_PROMOTION']]]
   end   
   
 end
